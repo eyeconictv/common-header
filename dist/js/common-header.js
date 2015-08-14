f3f10fe832addd3c276dbd42912957d22145335a
Oleg Khasimkhanov <oleg.khasimkhanov@risevision.com>
2015-08-14 15:20:01 -0400

    Fix browser navigation history

--- dist/js/common-header.js
+++ dist/js/common-header.js
@@ -1418,6 +1418,7 @@ angular.module("risevision.common.header", [
     //detect selectCompany changes on route UI
     $rootScope.$on("$stateChangeSuccess", selectedCompanyUrlHandler.updateSelectedCompanyFromUrl);
     $rootScope.$on("$routeChangeSuccess", selectedCompanyUrlHandler.updateSelectedCompanyFromUrl);
+    $rootScope.$on("$locationChangeSuccess", selectedCompanyUrlHandler.locationChangeSuccess);
   }
 ])
 
@@ -5117,10 +5118,43 @@ angular.module("risevision.common.header")
           newCompanyId !== userState.getSelectedCompanyId()) {
           userState.switchCompany(newCompanyId);
         } else if (!newCompanyId && userState.getSelectedCompanyId()) {
+          var currentURL = $location.absUrl();
           $location.search("cid", userState.getSelectedCompanyId());
-          $location.replace();
+          if (currentURL === $location.destUrl) {
+            // see explanation below
+            $location.replace();
+          }
         }
       };
+
+      this.locationChangeSuccess = function (event, newUrl) {
+        $location.destUrl = newUrl;
+      };
+
+      /*
+
+      Explanation for the usage of the $location.replace() above
+      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+      Scenario 1: When application is using "ng-href" directive, then application goes through the following cycle
+
+        $locationChangeSuccess -> $stateChangeSuccess -> $locationChangeSuccess
+
+      Scenario 2: When application is using "ui-sref" directive or "$state.go" funtion, then application goes through the following cycle
+
+        $stateChangeSuccess -> $locationChangeSuccess
+
+      Here is the dilemma:
+      - without $location.replace(), scenarion #2 works as expected creating single entries in the browser navigation history, 
+      however scenario #1 creates duplicate entries - one URL without "cid" parameter and with "cid".
+      - with $location.replace(), scenarion #2 does not add any entries to the browser navigation history, 
+      however scenario #1 works as expected.
+      
+      The solution is to monitor $locationChangeSuccess events and record "newUrl" parameter, then use it in $stateChangeSuccess event
+      in order to detect if pattern falls under scenario 1 or 2 then call $location.replace() based on that condition.
+
+      */
+
     }
   ]);
 })(angular);
