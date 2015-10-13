57332918179839c9bc3d97a67887eee5186f56c2
Alex Deaconu <alex.deaconu@gmail.com>
2015-10-13 16:14:26 -0400

    Unify tests into one file
    Renamed to homepage to be consistent with other apps
    Created common-scenarios which runs all 'logged in' tests
    
    Remainder of tests need to be integrated into common-scenarios,
    but currently they require the user to be logged out
    
    Added verification that secondary (jekins1) credential is present
    Trying to fix issue where jenkins company could be deleted

--- test/e2e/authentication-scenarios.js
+++ test/e2e/authentication-scenarios.js
@@ -5,17 +5,17 @@
   var expect = require('rv-common-e2e').expect;
   var assert = require('rv-common-e2e').assert;
   var CommonHeaderPage = require('rv-common-e2e').commonHeaderPage;
-  var CommonHeaderMenuPage = require('./pages/commonHeaderMenuPage.js');
+  var HomePage = require('./pages/homepage.js');
   var helper = require('rv-common-e2e').helper;
 
   browser.driver.manage().window().setSize(1280, 768);
 
   describe("Authentication", function() {
     this.timeout(2000);// to allow for protactor to load the seperate page
-    var commonHeaderPage, commonHeaderMenuPage;
+    var commonHeaderPage, homepage;
     before(function (){
       commonHeaderPage = new CommonHeaderPage();
-      commonHeaderMenuPage = new CommonHeaderMenuPage();
+      homepage = new HomePage();
 
       browser.get("http://localhost:8099/test/e2e");
     });
@@ -37,23 +37,23 @@
       helper.waitDisappear(commonHeaderPage.getLoader(), 'CH spinner loader');
 
       assert.eventually.isFalse(commonHeaderPage.getSignInButton().isDisplayed(), "sign in button should not show");
-      assert.eventually.isTrue(commonHeaderMenuPage.getProfilePic().isDisplayed(), "profile pic should show");
-      assert.eventually.isFalse(commonHeaderMenuPage.getSignOutButton().isDisplayed(), "sign out button should not show");
+      assert.eventually.isTrue(homepage.getProfilePic().isDisplayed(), "profile pic should show");
+      assert.eventually.isFalse(homepage.getSignOutButton().isDisplayed(), "sign out button should not show");
     });
 
     it("should log out", function() {
-      commonHeaderMenuPage.getProfilePic().click();
+      homepage.getProfilePic().click();
 
       //shows sign-out menu item
-      expect(commonHeaderMenuPage.getSignOutButton().isDisplayed()).to.eventually.be.true;
+      expect(homepage.getSignOutButton().isDisplayed()).to.eventually.be.true;
 
       //click sign out
-      commonHeaderMenuPage.getSignOutButton().click();
+      homepage.getSignOutButton().click();
       
-      helper.wait(commonHeaderMenuPage.getSignOutModal(), 'Sign Out Modal');
+      helper.wait(homepage.getSignOutModal(), 'Sign Out Modal');
       
-      assert.eventually.isTrue(commonHeaderMenuPage.getSignOutModal().isDisplayed(), "sign-out dialog should show");
-      commonHeaderMenuPage.getSignOutRvOnlyButton().click();
+      assert.eventually.isTrue(homepage.getSignOutModal().isDisplayed(), "sign-out dialog should show");
+      homepage.getSignOutRvOnlyButton().click();
 
       //signed out; sign-in button shows
       expect(commonHeaderPage.getSignInButton().isDisplayed()).to.eventually.equal(true);
---
+++ test/e2e/cases/user-settings.js
@@ -0,0 +1,111 @@
+(function() {
+
+  "use strict";
+  
+  var expect = require('rv-common-e2e').expect;
+  var assert = require('rv-common-e2e').assert;
+  var CommonHeaderPage = require('rv-common-e2e').commonHeaderPage;
+  var HomePage = require('./../pages/homepage.js');
+  var UserSettingsModalPage = require('./../pages/userSettingsModalPage.js');
+  var helper = require('rv-common-e2e').helper;
+
+  var UserSettingsScenarios = function() {
+    browser.driver.manage().window().setSize(1280, 768);
+
+    describe("User Settings", function() {
+      this.timeout(2000);// to allow for protactor to load the seperate page
+      var commonHeaderPage, 
+        homepage, 
+        userSettingsModalPage;
+        
+      before(function (){
+        commonHeaderPage = new CommonHeaderPage();
+        homepage = new HomePage();
+        userSettingsModalPage = new UserSettingsModalPage();
+
+        homepage.get();
+
+        //sign in, wait for spinner to go away
+        helper.waitDisappear(commonHeaderPage.getLoader(), 'CH spinner loader').then(function () {
+          commonHeaderPage.signin();
+        });
+      });
+
+      it("should show user settings modal", function() {
+        homepage.getProfilePic().click();
+        
+        assert.eventually.isTrue(homepage.getUserSettingsButton().isDisplayed(), "User settings menu item should show");
+
+        //click on user settings button
+        homepage.getUserSettingsButton().click();
+        
+        helper.wait(userSettingsModalPage.getUserSettingsModal(), "User Settings Modal");
+
+        assert.eventually.isTrue(userSettingsModalPage.getUserSettingsModal()
+          .isDisplayed(), "User settings modal should show after clicking on menu item");
+      });
+      
+      it("should update settings", function() {
+        userSettingsModalPage.getFirstNameField().clear();
+        userSettingsModalPage.getFirstNameField().sendKeys("John");
+
+        userSettingsModalPage.getLastNameField().clear();
+        userSettingsModalPage.getLastNameField().sendKeys("Doe");
+
+        userSettingsModalPage.getPhoneField().clear();
+        userSettingsModalPage.getPhoneField().sendKeys("000-000-0000");
+
+        userSettingsModalPage.getEmailField().clear();
+        userSettingsModalPage.getEmailField().sendKeys("testmail@testmail.com");
+
+        if ( !userSettingsModalPage.getCeCheckbox().isSelected() )
+        {
+           userSettingsModalPage.getCeCheckbox().click();
+        }
+
+        if ( userSettingsModalPage.getPuCheckbox().isSelected() )
+        {
+           userSettingsModalPage.getPuCheckbox().click();
+        }
+
+        if ( !userSettingsModalPage.getDaCheckbox().isSelected() )
+        {
+           userSettingsModalPage.getDaCheckbox().click();
+        }
+
+        //click save button
+        userSettingsModalPage.getSaveButton().click();
+        
+        helper.waitRemoved(userSettingsModalPage.getUserSettingsModal(), "User Settings Modal");
+        
+      });
+      
+      it("should show updated information", function() {
+        homepage.getProfilePic().click();
+        homepage.getUserSettingsButton().click();
+        
+        helper.wait(userSettingsModalPage.getUserSettingsModal(), "User Settings Modal");
+
+        expect(userSettingsModalPage.getFirstNameField().getAttribute('value')).to.eventually.equal("John");
+        expect(userSettingsModalPage.getLastNameField().getAttribute('value')).to.eventually.equal("Doe");
+        expect(userSettingsModalPage.getEmailField().getAttribute('value')).to.eventually.equal("testmail@testmail.com");
+        expect(userSettingsModalPage.getPhoneField().getAttribute('value')).to.eventually.equal("000-000-0000");
+
+        expect(userSettingsModalPage.getCeCheckbox().isSelected()).to.eventually.equal.true;
+        expect(userSettingsModalPage.getPuCheckbox().isSelected()).to.eventually.equal.false;
+        expect(userSettingsModalPage.getDaCheckbox().isSelected()).to.eventually.equal.true;
+      });
+
+      // username should be shown here instead of email
+      // however that's not an editable field
+      xit("should immediately update fixes", function () {
+        assert.eventually.equal(element(
+          by.css("span.username")).getText(), "testmail@testmail.com",
+            "Username should update");
+      });
+    });
+  };
+  
+  module.exports = UserSettingsScenarios;
+  
+})();
