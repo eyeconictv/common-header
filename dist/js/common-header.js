497e9d36083e0e83c2e3db748aa4013739a9ee93
adriandd <adriandd@gmail.com>
2015-10-21 12:13:43 -0400

    Squash and Merge /fix/modal-modal-form 2

--- dist/js/common-header.js
+++ dist/js/common-header.js
@@ -1473,20 +1473,29 @@ angular.module("risevision.common.header", [
     segmentAnalytics.load(SEGMENT_API_KEY, ENABLE_INTERCOM_MESSAGING);
   }
 ])
+  .directive("ngEnter", function () {
+    return function (scope, element, attrs) {
+      element.bind("keydown keypress", function (event) {
+        if (event.which === 13) {
+          scope.$apply(function () {
+            scope.$eval(attrs.ngEnter);
+          });
 
-.directive("ngEnter", function () {
-  return function (scope, element, attrs) {
-    element.bind("keydown keypress", function (event) {
-      if (event.which === 13) {
-        scope.$apply(function () {
-          scope.$eval(attrs.ngEnter);
+          event.preventDefault();
+        }
+      });
+    };
+  })
+  .directive("stopEvent", function () {
+    return {
+      restrict: "A",
+      link: function (scope, element, attr) {
+        element.on(attr.stopEvent, function (e) {
+          e.stopPropagation();
         });
-
-        event.preventDefault();
       }
-    });
-  };
-});
+    };
+  });
 
 angular.module("risevision.common.header")
   .directive("integerParser", [
