56c93f02c23bafdaccf50e62fccfa600575f795c
Alex Deaconu <alex.deaconu@risevision.com>
2016-12-12 13:50:06 -0500

    Update angular to 1.5.9; fix common-i18n issue (#643)
    * Use angular 1.5.9
    
    * Fix integer parser issue

--- src/js/directives/dtv-integer-parser.js
+++ src/js/directives/dtv-integer-parser.js
@@ -5,8 +5,11 @@ angular.module("risevision.common.header.directives")
       return {
         require: "ngModel",
         link: function (scope, ele, attr, ctrl) {
-          ctrl.$parsers.unshift(function (viewValue) {
+          ctrl.$parsers.push(function (viewValue) {
             return parseInt(viewValue, 10) || 0;
+          });
+          ctrl.$formatters.push(function (viewValue) {
+            return typeof viewValue === "undefined" ? "" : "" + viewValue;
           });
         }
       };
