49e7c744fcebaf437cda22c2a738c37159f1b8e6
Alex Deaconu <alex.deaconu@risevision.com>
2017-10-27 14:38:21 -0400

    Storage/Store in RVA frame fixes (#721)

--- dist/js/common-header.js
+++ dist/js/common-header.js
@@ -7045,9 +7045,7 @@ angular.module("risevision.common.components.logging")
               getBaseDomain()
           };
 
-          if (_state.userToken !== "dummy") {
-            opts.authuser = _state.userToken.email;
-          } else {
+          if (_state.userToken === "dummy") {
             opts.authuser = $http.get(
               "https://www.googleapis.com/oauth2/v1/userinfo?access_token=" +
               _state.params.access_token)
@@ -7057,6 +7055,8 @@ angular.module("risevision.common.components.logging")
                 $log.debug("Error retrieving userinfo");
                 return opts.authuser;
               });
+          } else if (_state.userToken) {
+            opts.authuser = _state.userToken.email;
           }
 
           if (attemptImmediate) {
@@ -7160,7 +7160,8 @@ angular.module("risevision.common.components.logging")
         };
 
         var googleAuthFactory = {
-          authenticate: userState._state.inRVAFrame ?
+          authenticate: userState._state.inRVAFrame ||
+            ($window.self !== $window.top) ?
             authenticate : authenticateRedirect
         };
 
