c6e42f460ebe1586326cea1a1a92b388445396c2
Alex Deaconu <alex.deaconu@risevision.com>
2019-01-14 09:24:36 -0500

    Allow user to Subscribe to a Plan (#906)
    When a Trial is available
    
    Update styling to align button and link to bottom

--- test/e2e/pages/plansModalPage.js
+++ test/e2e/pages/plansModalPage.js
@@ -13,7 +13,7 @@
     function getButtonByPlan(selector, plan) {
       var idx = plansList.indexOf(plan) + 1;
 
-      return element(by.css("#planHeader > div:nth-child(" + idx + ") > #" + selector));
+      return element(by.css("#planHeader > div:nth-child(" + idx + ")")).element(by.id(selector));
     }
 
     this.getCurrentPlanButton = function(plan) {
