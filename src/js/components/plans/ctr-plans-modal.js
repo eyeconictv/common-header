d980794d79a09812f398bec10d7c4ab6c09fcc4b
Tyler Johnson <mail@tyler-johnson.ca>
2019-06-18 22:44:47 -0400

    Add directive for custom event listeners
    When the polymer component updates display count property to a valid
    value, the digest cycle runs. But for an invalid value of 0 the
    digest cycle does not run and the button is left enabled until the input
    field loses focus. With this change, the button is set to disabled as soon
    as the input is set to 0.
    
    This is a nice collaboration between Polymer's "notify" feature to send
    changed events up to containing elements and angular's custom
    directive capability. This custom directive
    wouldn't be necessary at all in angular 1.7.3 which includes ngOn.

--- src/js/components/plans/ctr-plans-modal.js
+++ src/js/components/plans/ctr-plans-modal.js
@@ -1,5 +1,20 @@
 angular.module("risevision.common.components.plans")
+.directive("rvOn", function () {
+  return {
+    restrict: "A",
+    link: function (scope, element, attrs) {
+      var attrVal = attrs.rvOn;
+      var eventName = attrVal.split(":")[0];
+      var ctrlFn = attrVal.split(":")[1];
 
+      element.on(eventName, function (event) {
+        scope.$apply(scope[ctrlFn].bind(null, scope, {
+          $event: event
+        }));
+      });
+    }
+  };
+})
 .controller("PlansModalCtrl", [
   "$scope", "$rootScope", "$modalInstance", "$log", "$loading", "$timeout",
   "plansFactory", "currentPlanFactory", "ChargebeeFactory", "userState", "purchaseFactory",
