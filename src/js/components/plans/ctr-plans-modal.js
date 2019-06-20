2739ca6e860049e99e4493c65d1705f36638ffbe
Tyler Johnson <mail@tyler-johnson.ca>
2019-06-20 07:09:20 -0400

    Move rv-on directive to separate file

--- src/js/components/plans/ctr-plans-modal.js
+++ src/js/components/plans/ctr-plans-modal.js
@@ -1,20 +1,4 @@
 angular.module("risevision.common.components.plans")
-  .directive("rvOn", function () {
-    return {
-      restrict: "A",
-      link: function (scope, element, attrs) {
-        var attrVal = attrs.rvOn;
-        var eventName = attrVal.split(":")[0];
-        var ctrlFn = attrVal.split(":")[1];
-
-        element.on(eventName, function (event) {
-          scope.$apply(scope[ctrlFn].bind(null, scope, {
-            $event: event
-          }));
-        });
-      }
-    };
-  })
   .controller("PlansModalCtrl", [
     "$scope", "$rootScope", "$modalInstance", "$log", "$loading", "$timeout", "getCompany",
     "plansFactory", "currentPlanFactory", "ChargebeeFactory", "userState", "purchaseFactory",
