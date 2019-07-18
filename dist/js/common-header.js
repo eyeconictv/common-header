576f43f455e2490bf0f47e80385e40f739e01b3a
Alex Deaconu <alex.deaconu@gmail.com>
2019-07-18 17:23:49 -0400

    Add directive that checks and sets CH height (#1013)
    Used for Editor and Template Editor
    It sets the height based on the actual value
    Refresh values on Company update/change and Plan change

--- dist/js/common-header.js
+++ dist/js/common-header.js
@@ -518,6 +518,37 @@ angular.module("risevision.common.header.directives")
 "use strict";
 
 angular.module("risevision.common.header.directives")
+  .directive("commonHeaderHeight", ["$rootScope", "$window", "$timeout",
+    function ($rootScope, $window, $timeout) {
+      return {
+        restrict: "A",
+        scope: false,
+        compile: function (elem) {
+          var _updateHeight = function () {
+            // Wait for the digest cycle to finish updating the UI
+            $timeout(function () {
+              var commonHeaderDiv = $window.document.getElementById("commonHeaderDiv");
+              var elemStyle = $window.getComputedStyle(commonHeaderDiv);
+              var currentHeightPx = elemStyle.getPropertyValue("height");
+              var currentHeight = parseInt(currentHeightPx);
+
+              elem[0].style.setProperty("--common-header-height", currentHeight + "px");
+            });
+          };
+
+          _updateHeight();
+
+          $rootScope.$on("risevision.company.selectedCompanyChanged", _updateHeight);
+          // Should take care of plan & company settings updates
+          $rootScope.$on("risevision.company.updated", _updateHeight);
+        } //link()
+      };
+    }
+  ]);
+
+"use strict";
+
+angular.module("risevision.common.header.directives")
   .directive("companyButtons", ["$templateCache",
     function ($templateCache) {
       return {
